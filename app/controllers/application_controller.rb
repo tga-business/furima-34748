ef0b569ab872988c504966e41458626e608d6460
tga-business <81005306+tga-business@users.noreply.github.com>
2021-05-29 18:34:05 +0900

    basic認証実装（途中）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  private
+  def basic_auth
+    authenticate_orrequest_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"], && password == ENV[BASIC_AUTH_PASSWORD]
+  end
 end
+
