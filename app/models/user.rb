6ccfc8c08624ee391c5cf230c0f3863d2dc09069
tga-business <81005306+tga-business@users.noreply.github.com>
2021-06-19 16:38:33 +0900

    ログイン、ログアウト機能実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :email, uniqueness: true
+         validates :password, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+         validates :first_name, format:{with: /\A([ぁ-んァ-ヶ一-龥々ー])+\z/}
+         validates :last_name,  format:{with: /\A([ぁ-んァ-ヶ一-龥々ー])+\z/}
+         validates :first_kana, format:{with: /\A([ァ-ヶ一ー])+\z/}
+         validates :last_kana,  format:{with: /\A([ァ-ヶ一ー])+\z/}
+         validates :birth, presence: true
+
+         has_many :products
 end
