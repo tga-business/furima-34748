b5cfc213277468d9521a553987ed56c935001369
tga-business <81005306+tga-business@users.noreply.github.com>
2021-06-22 12:25:00 +0900

    バリデーション編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,21 @@ class User < ApplicationRecord
          with_options presence:true do
           validates :nickname
           validates :birth
+          validates :first_name
+          validates :last_name
+          validates :first_kana
+          validates :last_kana
          end
          validates :email, uniqueness: true
          validates :password, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-         validates :first_name, format:{with: /\A([ぁ-んァ-ヶ一-龥々ー])+\z/}
-         validates :last_name,  format:{with: /\A([ぁ-んァ-ヶ一-龥々ー])+\z/}
-         validates :first_kana, format:{with: /\A([ァ-ヶ一ー])+\z/}
-         validates :last_kana,  format:{with: /\A([ァ-ヶ一ー])+\z/}
+         with_options  format:{with: /\A([ぁ-んァ-ヶ一-龥々ー])+\z/} do
+          validates :first_name
+          validates :last_name
+         end
+         with_options format:{with: /\A([ァ-ヶ一ー])+\z/} do
+           validates :first_kana
+           validates :last_kana
+         end
 
          
          has_many :products
