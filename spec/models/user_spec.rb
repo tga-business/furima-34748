4492728de9ebaf5548f54f89f706600d925f2729
tga-business <81005306+tga-business@users.noreply.github.com>
2021-06-22 13:02:29 +0900

    細部の編集

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,12 +27,24 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
 
+    it '重複したemailが存在する場合登録できないこと' do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
+
+    it 'emailが空では登録できないこと' do
+      @user.email = 'testmailaddres'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
+    end
+
     it 'passwordが空では登録できないこと' do
       @user.password = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Password can't be blank")
     end
-
 
     it 'passwordが5文字以下であれば登録できないこと' do
       @user.password = '12345'
@@ -69,12 +81,6 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
 
-    it '重複したemailが存在する場合登録できないこと' do
-      @user.save
-      another_user = FactoryBot.build(:user, email: @user.email)
-      another_user.valid?
-      expect(another_user.errors.full_messages).to include('Email has already been taken')
-    end
 
     it 'last_nameが空では登録できないこと'do
      @user.last_name = ''
